d6d5df1ccc2690fc53a9a06e664994199df4e1ba
Gabriel Sales <99056850+GabrielSales10@users.noreply.github.com>
2025-09-13 15:10:44 -0300

    Remove unnecessary comment from CandidatosManager

--- src/components/admin/CandidatosManager.tsx
+++ src/components/admin/CandidatosManager.tsx
@@ -1,4 +1,3 @@
-// src/components/admin/CandidatosManager.tsx
 import MultiYearCandidateManager from "./MultiYearCandidateManager";
 export default function CandidatosManager() {
   return <MultiYearCandidateManager />;
