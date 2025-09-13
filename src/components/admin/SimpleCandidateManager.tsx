65bb6f95f2c95cdef71360d6b431b0b1d0f7f045
Gabriel Sales <99056850+GabrielSales10@users.noreply.github.com>
2025-09-13 15:11:08 -0300

    Remove unnecessary comment from SimpleCandidateManager

--- src/components/admin/SimpleCandidateManager.tsx
+++ src/components/admin/SimpleCandidateManager.tsx
@@ -1,4 +1,3 @@
-// src/components/admin/SimpleCandidateManager.tsx
 import MultiYearCandidateManager from "./MultiYearCandidateManager";
 export default function SimpleCandidateManager() {
   return <MultiYearCandidateManager />;
